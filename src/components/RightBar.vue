ac4f7fce0e74d991671a0e834d8ec3e4f3856742
borcesmileski <borce.smileski@gmail.com>
2022-07-08 10:04:09 +0200

    swap on right bar action

--- src/components/RightBar.vue
+++ src/components/RightBar.vue
@@ -15,21 +15,25 @@
     </div>
     <hr style="color: #820f82;">
     <div class="mt-2">
-      <h4 class="pb-2 text-truncate">Competences <i class="fa fa-arrow-down text-primary" v-on:click="slipDown" aria-hidden="true"></i></h4>
-      <div v-for="comp in competences">
+      <h4 class="pb-2 text-truncate ">Competences <i class="fa fa-arrow-down text-primary"
+                                                     v-on:click="slipDown" aria-hidden="true"></i></h4>
+      <div v-for="comp in competences" v-if="boolComp">
         <p>{{ comp.skill }} <i style="color:#820f82 " class="fa fa-check " aria-hidden="true"></i></p>
       </div>
 
     </div>
-    <h4>Soft skills</h4>
-    <div class="row" v-for="skill in skills">
-      <div class="col-4 col-lg-6 pt-2">
-        {{skill.name}}
-      </div>
-      <div class="col-6 col-lg-4">
+    <div>
+      <h4>Soft skills <i class="fa fa-arrow-down text-primary" v-on:click="slipDown1"
+                         aria-hidden="true"></i></h4>
+      <div class="row" v-for="skill in skills" v-if="boolComp1">
+        <div class="col-4 col-lg-6 pt-2">
+          {{ skill.name }}
+        </div>
+        <div class="col-6 col-lg-4">
           <span v-for="i in skill.stars">
             <i class="text-primary fa fa-circle" aria-hidden="true"></i>
           </span>
+        </div>
       </div>
     </div>
   </div>
@@ -38,9 +42,21 @@
 <script>
 export default {
   name: "RightBar",
-  methods:{
-    slipDown(){
-      console.log('d')
+  methods: {
+    slipDown() {
+      if (this.boolComp == true) {
+        this.boolComp = false;
+      } else {
+        this.boolComp = true;
+      }
+    },
+    slipDown1() {
+      console.log("slipdown1")
+      if (this.boolComp1 == true) {
+        this.boolComp1 = false;
+      } else {
+        this.boolComp1 = true;
+      }
     }
   },
   data() {
@@ -65,52 +81,54 @@ export default {
           skill: ' Great communication skills',
         }
       ],
-      skills:[
+      skills: [
         {
-          name:'Adaptability and flexibility',
-          stars:5
+          name: 'Adaptability and flexibility',
+          stars: 5
         },
         {
-          name:'Accepting and giving feedback',
-          stars:5
+          name: 'Accepting and giving feedback',
+          stars: 5
         },
         {
-          name:'Logical thinking',
-          stars:5
+          name: 'Logical thinking',
+          stars: 5
         },
         {
-          name:'Teamwork',
-          stars:5
+          name: 'Teamwork',
+          stars: 5
         },
         {
-          name:'Curiosity',
-          stars:5
+          name: 'Curiosity',
+          stars: 5
         },
         {
-          name:'Willingness to learn',
-          stars:5
+          name: 'Willingness to learn',
+          stars: 5
         },
         {
-          name:'Communication',
-          stars:5
+          name: 'Communication',
+          stars: 5
         },
         {
-          name:'Problem Solving',
-          stars:5
+          name: 'Problem Solving',
+          stars: 5
         },
         {
-          name:'TimeManagement',
-          stars:5
+          name: 'TimeManagement',
+          stars: 5
         },
         {
-          name:'Working well under pressure',
-          stars:5
+          name: 'Working well under pressure',
+          stars: 5
         },
         {
-          name:'Leadership',
-          stars:5
+          name: 'Leadership',
+          stars: 5
         }
-      ]
+      ],
+      boolComp: true,
+      boolComp1: true,
     }
   }
 }
